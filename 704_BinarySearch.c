9fb482655e09b49efdd0358e3d263c9231479fec
xiaoxy <xiaoxiaoyu0226@gmail.com>
2024-04-11 19:23:20 -0400

    leetcode25

--- 704_BinarySearch.c
+++ 704_BinarySearch.c
@@ -1,15 +1,22 @@
 //Problem: 704. Binary Search
 //Easy 1pts
-//Links: https://leetcode.com/problems/binary-search/description/
+
+//Topics: Array, Binary Search
+
 //Description: Given an array of integers nums which is sorted in ascending order, 
 //and an integer target, write a function to search target in nums. 
 //If target exists, then return its index. Otherwise, return -1.
+
+//Links: https://leetcode.com/problems/binary-search/description/
+
 
 int search(int* nums, int numsSize, int target) {
     int left = 0;
     int right = numsSize -1;
 
+    
     while (left <= right){
+        //define the mid point
         int mid = left + (right - left)/2;
 
         if (nums[mid] == target)
