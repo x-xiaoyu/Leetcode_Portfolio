feac38fe76a24cebb36ce5e54214ec4105f246cf
xiaoxy <xiaoxiaoyu0226@gmail.com>
2024-04-15 02:31:04 -0400

    comments

---
+++ 21_MergeTwoSortedLists.c
@@ -0,0 +1,10 @@
+// Problem: 21.merge-two-sorted-lists
+// Easy 1pts
+// 04/12/2024
+// Topics: Linked List, Recursion
+
+// Description: You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// Return the head of the merged linked list.
+
+// Links: hhttps://leetcode.com/problems/merge-two-sorted-lists/description/
